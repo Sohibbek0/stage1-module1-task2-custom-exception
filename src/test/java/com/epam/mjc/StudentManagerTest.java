fe3b6515095ea6a52b76808a528876aabbfc6ffe
Sohibbek0 <Sohibbek99@gmail.com>
2024-09-13 12:47:17 +0300

    Завершил задачу

--- src/test/java/com/epam/mjc/StudentManagerTest.java
+++ src/test/java/com/epam/mjc/StudentManagerTest.java
@@ -3,50 +3,46 @@ package com.epam.mjc;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 
-
 public class StudentManagerTest {
-  StudentManager manager = new StudentManager();
+    StudentManager manager = new StudentManager();
 
-  @Test(expected = IllegalArgumentException.class)
-  public void findNotValid() throws IllegalArgumentException {
-    manager.find(1000);
-  }
-
-  @Test
-  public void findValidStudent() {
-    try {
-      assertNotNull(manager.find(1));
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
+    @Test(expected = StudentNotFoundException.class)
+    public void findNotValid() {
+        manager.find(1000);
     }
-  }
 
-  @Test
-  public void testExceptionMessage() {
-    try {
-      assertNotNull(manager.find(1000));
-    } catch (IllegalArgumentException e) {
-      assertEquals("Could not find student with ID 1000", e.getMessage());
+    @Test
+    public void findValidStudent() {
+        try {
+            assertNotNull(manager.find(1));
+        } catch (StudentNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  @Test
-  public void testIDsNotChangedV1() {
-    try {
-      assertNull("Student enum should be have only 10 values.", manager.find(11));
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
+    @Test
+    public void testExceptionMessage() {
+        try {
+            manager.find(1000);
+        } catch (StudentNotFoundException e) {
+            assertEquals("Could not find student with ID 1000", e.getMessage());
+        }
     }
-  }
 
-  @Test
-  public void testIDsNotChangedV2() {
-    assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+    @Test
+    public void testIDsNotChangedV1() {
+        try {
+            assertNull(manager.find(11));
+        } catch (StudentNotFoundException e) {
+            // Expected exception
+        }
+    }
 
-  }
-
+    @Test
+    public void testIDsNotChangedV2() {
+        assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+    }
 }
